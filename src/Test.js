9c04efa95c4ad880c23007c685cb83c8d7ab35d7
Praveen Voruganti -X (prvoruga - ITALENT APPLICATIONS PRIVATE LIMITED at Cisco) <prvoruga@cisco.com>
2024-03-19 23:08:02 +0530

    test

--- src/Test.js
+++ src/Test.js
@@ -6,14 +6,12 @@ export default function Test() {
     function TestFunction(){
         console.log("Test Function Called")
         setdata("Yes i Am Button")
-
     }
   return (
     <div>
         <center>
             <button onClick={TestFunction}>Click me</button>
             <h1>{data}</h1>
-            <h2>{data1}</h2>
         </center>
     </div>
   )
